54a82bf5de5cd53a68776565f0ff75b61a22553e
RobertoVM94 <r.vicario.martin@gmail.com>
2025-04-30 17:35:46 +0200

    Delete Frontend directory

--- Frontend/src/components/TasksBox.js
+++
@@ -1,9 +0,0 @@
-export default function TasksBox({name, done, id}) {
-    return (
-        <article> 
-            <h3>{name}</h3>
-            <p>{done ? 'Pendiente':'Hecho' }</p>
-            <p>ID: {id}</p>
-        </article>
-    )
-}
